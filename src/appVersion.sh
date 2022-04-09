52fb128b7d2dd300146db9ffb4213d5ea69bf8e5
alexanderharm <contact@aharm.de>
2022-04-09 02:27:31 +0200

    fallback to other version strings

--- src/appVersion.sh
+++ src/appVersion.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-defaults read "${1}" CFBundleShortVersionString
+if defaults read "${1}" CFBundleShortVersionString > /dev/null 2>&1; then
+    defaults read "${1}" CFBundleShortVersionString
+    exit
+elif defaults read "${1}" CFBundleVersion > /dev/null 2>&1; then
+    defaults read "${1}" CFBundleVersion
+    exit
+else
+    defaults read "${1}" CFBundleInfoDictionaryVersion
+fi
