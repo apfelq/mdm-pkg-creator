ec5f58eebb4d7f141bbeaf70ad63e30b4a1b902e
alexanderharm <contact@aharm.de>
2022-04-09 02:21:34 +0200

    uri encode the cdn's to ease copy/paste

--- src/index.ts
+++ src/index.ts
@@ -252,7 +252,7 @@ async function main ()
                     appCodeRequirement: configApps[update].appCodeRequirement,
                     appName: configApps[update].appName,
                     appVersion: configApps[update].appVersion,
-                    cdn: config.cdn.map(server => `${server}${update}_${configApps[update].appVersion}.pkg`),
+                    cdn: config.cdn.map(server => encodeURI(`${server}${update}_${configApps[update].appVersion}.pkg`)),
                     pkgChecksum: configApps[update].pkgChecksum,
                     pkgSigned: configApps[update].pkgSigned
                 }
